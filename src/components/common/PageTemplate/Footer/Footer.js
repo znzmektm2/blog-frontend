c519ae4f421ba649a57050debbf954d1d53322e2
znzmektm2 <tingcber2@naver.com>
2020-04-08 12:29:25 +0900

    add: Sass 및 CSS 모듈 적용, 라우터 & 리덕스 적용

---
+++ src/components/common/PageTemplate/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Footer.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const Footer = () => {
+  return (
+    <div>
+      Footer
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/common/PageTemplate/PageTemplate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './PageTemplate.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const PageTemplate = () => {
+  return (
+    <div className={cx('page-template')}>
+      PageTemplate
+    </div>
+  );
+};
+
+export default PageTemplate;
