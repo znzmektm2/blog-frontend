cfbf3566d59086d47cd30adea66e3b7d00e57217
znzmektm2 <tingcber2@naver.com>
2020-04-17 02:07:31 +0900

    fix: 오타 수정

--- src/components/common/MarkdownRender/MarkdownRender.js
+++ src/components/common/MarkdownRender/MarkdownRender.js
@@ -21,7 +21,7 @@ class MarkdownRender extends Component {
     html: ''
   }
 
-  renderMarkdon = () => {
+  renderMarkdown = () => {
     const { markdown } = this.props;
     // 마크다운이 존재하지 않는다면 공백 처리
     if(!markdown) {
@@ -38,17 +38,17 @@ class MarkdownRender extends Component {
 
   constructor(props) {
     super(props);
-    const { markdown } = props;
+    const { markdown } = this.props;
     // 서버사이드 렌더링에서도 마크다운 처리가 되도록 constructor 쪽에서도 구현합니다.
     this.state = {
-      html: markdown ? marked(props.markdown, { breaks: true, sanitize: true }) : ''
+      html: markdown ? marked(markdown, { breaks: true, sanitize: true }) : ''
     } 
   }
 
   componentDidUpdate(prevProps, prevState) {
     // markdown 값이 변경되면 renderMarkdown을 호출합니다.
     if(prevProps.markdown !== this.props.markdown) {
-      this.renderMarkdon();
+      this.renderMarkdown();
     }
     // state가 바뀌는 코드 하이라이팅
     if(prevState.html !== this.state.html) {
@@ -62,9 +62,7 @@ class MarkdownRender extends Component {
     // React에서 html을 렌더링하려면 객체를 만들어 내부에 __html 값을 설정해야 합니다.
     const markup = {
       __html: html
-    };
-    
-  
+    };   
 
     //그리고 dangerouslySetInnerHTML 값에 해당 객체를 넣어 주면 됩니다.
     return (
